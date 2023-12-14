b61a5493a1315af8285f52cb5fee5e7e2cfaf758
sunjin <changido2888@gmail.com>
2023-12-14 22:07:27 +0900

    1214

---
+++ section7/src/chapter0.ts
@@ -0,0 +1,21 @@
+/**
+ * 제너릭 :일반적인, 포괄적인
+ */
+
+// 제네릭 함수 : 모든 타입에 다 쓸수 있는 함수 , T: 타입변수
+function func<T>(value: T): T {
+  return value;
+}
+
+let num = func(10);
+// num.toUpperCase();
+
+if (typeof num === "number") {
+  num.toFixed();
+}
+
+let bool = func(true);
+
+let str = func("string");
+
+let arr = func<[number, number, number]>([1, 2, 3]);
